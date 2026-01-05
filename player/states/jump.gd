749510326bb793b537e3452cee45addb8d6e178f
Mihail Jordanov <103178509+BojeMoii@users.noreply.github.com>
2026-01-05 20:16:27 +0200

    Make 3 new Levels and new sprites

---
+++ player/states/jump.gd
@@ -0,0 +1,50 @@
+class_name PlayerStateJump extends PlayerState
+
+@export var jump_velocity : float = 450.0
+
+
+func init() -> void:
+	pass
+	
+func enter() -> void:
+	player.animation_player.play("jump")
+	player.animation_player.pause()
+	#player.add_debug_indicator( Color.LIME_GREEN )
+	player.velocity.y -= jump_velocity
+	
+	if player.previouse_state == fall and not Input.is_action_pressed( "jump" ):
+		await get_tree().physics_frame
+		player.velocity.y *= 0.5
+		player.change_state( fall )
+		pass
+			
+	pass
+	
+func exit() -> void:
+	#player.add_debug_indicator( Color.YELLOW )
+	pass
+	
+	
+func handle_input( event : InputEvent ) -> PlayerState:
+	if event.is_action_pressed("jump"):
+		player.velocity.y *= 0.5
+		return fall
+	return next_state
+
+
+func process( _delta: float ) -> PlayerState:
+	set_jump_frame()
+	return next_state
+	
+func physics_process(delta: float) -> PlayerState:
+	if player.is_on_floor():
+		return idle
+	if player.velocity.y >= 0:
+		return fall
+	player.velocity.x = player.direction.x * player.move_speed
+	return next_state
+	
+func set_jump_frame() -> void:
+	var frame : float = remap( player.velocity.y, -jump_velocity, 0.0, 0.0, 0.5 )
+	player.animation_player.seek( frame, true )
+	pass
