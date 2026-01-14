f5ce2915032bf2bec60231c3e9c2be46796bffc4
Mihail Jordanov <103178509+BojeMoii@users.noreply.github.com>
2026-01-14 16:46:30 +0200

    Add Pause Menu

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -56,6 +56,12 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("action"):
 		Messages.player_interact.emit( self )
+	elif event.is_action_pressed( "pause" ):
+		get_tree().paused = true
+		var pasue_menu : PauseMenu = load( "res://pause_menu/pause_menu.tscn" ).instantiate()
+		add_child( pasue_menu )
+		return
+	
 		
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_MINUS:
