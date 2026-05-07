95c77d2586127df83d5d78d05de99c256da9a2b9
Mihail Jordanov <103178509+BojeMoii@users.noreply.github.com>
2026-05-07 16:49:51 +0300

    Slime Enemy

--- player/scripts/player_camera.gd
+++ player/scripts/player_camera.gd
@@ -6,6 +6,7 @@ var shake_strength : float = 0.0
 
 func _ready() -> void:
 	VisualEffects.camera_shook.connect( _apply_shake )
+	SceneManager.new_scene_ready.connect( _on_scene_transition )
 	pass
 
 
@@ -19,4 +20,8 @@ func _process(delta: float) -> void:
 
 func _apply_shake( strength : float ) -> void:
 	shake_strength = min( strength, max_shake_offset )
+	pass
+	
+func _on_scene_transition( _t, _o ) -> void:
+	reset_smoothing.call_deferred()
 	pass
